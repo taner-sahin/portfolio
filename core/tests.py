233fb4b02d1096ddbd758963883e7902f9bab1f0
Taner Sahin <kemal.liya19@gmail.com>
2026-09-03 22:43:07 +0300

    test: use secure requests for production tests

--- core/tests.py
+++ core/tests.py
@@ -5,11 +5,15 @@ from django.urls import reverse
 class CorePageTests(TestCase):
 
     def test_home_page_returns_200(self):
-        response = self.client.get(reverse("core:home"))
+        response = self.client.get(
+            reverse("core:home"),
+            secure=True,
+        )
         self.assertEqual(response.status_code, 200)
 
     def test_case_study_page_returns_200(self):
         response = self.client.get(
-            reverse("core:careertrack_case_study")
+            reverse("core:careertrack_case_study"),
+            secure=True,
         )
         self.assertEqual(response.status_code, 200)
